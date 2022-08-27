51733e1de8f98db3a05b34f2835c27ce7912a816
Sidratul Muntaha Sara <sidratul.s@outlook.com>
2022-08-28 01:23:16 +0400

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,11 +20,19 @@ function myFunction() {
 let btn = document.querySelector('.play-btn');
 let clip = document.querySelector('.clip');
 let close = document.querySelector('.close');
+let video = document.querySelector('video');
 btn.onclick = function(){
-    btn.classList.add('active')
-    clip.classList.add('active')
+    btn.classList.add('active');
+    clip.classList.add('active');
+    video.play();
+    video.currentTime = 0;
 }
 close.onclick = function(){
-    btn.classList.remove('active')
+    btn.classList.remove('active');
+    clip.classList.remove('active');
+    video.pause();
+}
+video.onended = function(){
+  btn.classList.remove('active')
     clip.classList.remove('active')
 }
