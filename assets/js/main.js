aa5e646a2a45f2bec88cdb2efdbb4334df94b431
Sidratul Muntaha Sara <sidratul.s@outlook.com>
2022-08-29 06:50:05 +0400

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,118 @@ function openInfo() {
 function closeInfo() {
   document.getElementById("report").style.width = "0";
 }
+
+//Slider
+// I hope this over-commenting helps. Let's do this!
+// Let's use the 'active' variable to let us know when we're using it
+let active = false;
+// First we'll have to set up our event listeners
+// We want to watch for clicks on our scroller
+document.querySelector('.scroller-middle').addEventListener('mousedown',function(){
+  active = "middle";
+  // Add our scrolling class so the scroller has full opacity while active
+  document.querySelector('.scroller-middle').classList.add('scrolling');
+});
+// We also want to watch the body for changes to the state,
+// like moving around and releasing the click
+// so let's set up our event listeners
+document.body.addEventListener('mouseup',function(){
+  active = false;
+  document.querySelector('.scroller-middle').classList.remove('scrolling');
+});
+document.body.addEventListener('mouseleave',function(){
+  active = false;
+  document.querySelector('.scroller-middle').classList.remove('scrolling');
+});
+// We'll have to do the same for our top scroller
+document.querySelector('.scroller-top').addEventListener('mousedown',function(){
+    active = "top";
+    document.querySelector('.scroller-top').classList.add('scrolling');
+});
+document.body.addEventListener('mouseup',function(){
+  active = false;
+  document.querySelector('.scroller-top').classList.remove('scrolling');
+});
+document.body.addEventListener('mouseleave',function(){
+  active = false;
+  document.querySelector('.scroller-top').classList.remove('scrolling');
+});
+// Let's figure out where their mouse is at
+document.body.addEventListener('mousemove',function(e){
+  if (!active) return;
+  // Their mouse is here...
+  let x = e.pageX;
+  // but we want it relative to our wrapper
+  x -= document.querySelector('.wrapper').getBoundingClientRect().left;
+  // Okay let's change our state
+  scrollIt(x);
+});
+// Let's use this function
+function scrollIt(x){
+  // Calculate our transform
+  let transform = Math.max(0,(Math.min(x,document.querySelector('.wrapper').offsetWidth)));
+  // we show all our bottom image but how much of our middle and top,
+  // that'll depend on what we're dragging
+  // if we're dragging the middle slider
+  if (active==="middle"){
+    document.querySelector('.middle').style.width = transform+"px";
+    document.querySelector('.scroller-middle').style.left = transform-25+"px";
+    // if we're using scroller-middle, middle must always be to the right of top
+    if (document.querySelector('.scroller-top').getBoundingClientRect().left>document.querySelector('.scroller-middle').getBoundingClientRect().left-5){
+      document.querySelector('.top').style.width = transform-5+"px";
+      document.querySelector('.scroller-top').style.left = transform-30+"px";
+    }
+  }
+  // if we're dragging the top slider
+  if (active==="top"){
+    document.querySelector('.top').style.width = transform+"px";
+    document.querySelector('.scroller-top').style.left = transform-25+"px";
+    // if we're using scroller-top, top must always be to the left
+    if (document.querySelector('.scroller-top').getBoundingClientRect().left>document.querySelector('.scroller-middle').getBoundingClientRect().left-5){
+      document.querySelector('.middle').style.width = transform+5+"px";
+      document.querySelector('.scroller-middle').style.left = transform-20+"px";
+    }
+  }
+}
+// Let's set our opening state based off the width, 
+// we want to show a bit of both images so the user can see what's going on
+active = "middle";
+scrollIt(460);
+active = "top";
+scrollIt(230);
+active = false;
+
+// And finally let's repeat the process for touch events
+// first our middle scroller...
+document.querySelector('.scroller-middle').addEventListener('touchstart',function(){
+    active = "middle";
+    document.querySelector('.scroller-middle').classList.add('scrolling');
+});
+document.body.addEventListener('touchend',function(){
+    active = false;
+    document.querySelector('.scroller-middle').classList.remove('scrolling');
+});
+document.body.addEventListener('touchcancel',function(){
+    active = false;
+    document.querySelector('.scroller-middle').classList.remove('scrolling');
+});
+// then scroller top, our second scroller
+document.querySelector('.scroller-top').addEventListener('touchstart',function(){
+    active = "top";
+    document.querySelector('.scroller-top').classList.add('scrolling');
+});
+document.body.addEventListener('touchend',function(){
+    active = false;
+    document.querySelector('.scroller-top').classList.remove('scrolling');
+});
+document.body.addEventListener('touchcancel',function(){
+    active = false;
+    document.querySelector('.scroller-top').classList.remove('scrolling');
+});
+document.querySelector('.wrapper').addEventListener('touchmove',function(e){
+    if (!active) return;
+    e.preventDefault();
+    let x = e.touches[0].pageX;
+    x -= document.querySelector('.wrapper').getBoundingClientRect().left;
+    scrollIt(x);
+});
