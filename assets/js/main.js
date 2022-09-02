3ec6b1a4bf5c0bab3578cc0c78e443c7d2473eab
Sidratul Muntaha Sara <sidratul.s@outlook.com>
2022-09-02 16:08:06 +0400

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -306,3 +306,57 @@ document.getElementById('c2-h5').addEventListener('mouseover',setURL('../assets/
 document.getElementById('c2-h6').addEventListener('mouseover',setURL('../assets/imgs/env.jpg'));
 
 /* ---- =========================================================== ---- */
+
+
+
+	//Page cursors
+
+  document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+    t.style.left = n.clientX + "px", 
+t.style.top = n.clientY + "px", 
+e.style.left = n.clientX + "px", 
+e.style.top = n.clientY + "px", 
+i.style.left = n.clientX + "px", 
+i.style.top = n.clientY + "px"
+});
+var t = document.getElementById("cursor"),
+    e = document.getElementById("cursor2"),
+    i = document.getElementById("cursor3");
+function n(t) {
+    e.classList.add("hover"), i.classList.add("hover")
+}
+function s(t) {
+    e.classList.remove("hover"), i.classList.remove("hover")
+}
+s();
+for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+    o(r[a])
+}
+function o(t) {
+    t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+}
+
+$(document).ready(function(){"use strict";
+
+//Scroll indicator
+
+var progressPath = document.querySelector('.progress-wrap path');
+var pathLength = progressPath.getTotalLength();
+progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+progressPath.style.strokeDashoffset = pathLength;
+progressPath.getBoundingClientRect();
+progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
+var updateProgress = function () {
+  var scroll = $(window).scrollTop();
+  var height = $(document).height() - $(window).height();
+  var progress = pathLength - (scroll * pathLength / height);
+  progressPath.style.strokeDashoffset = progress;
+}
+updateProgress();
+$(window).scroll(updateProgress);
+
+
+});
+
+(jQuery); 
